7a50841f51abc22e75158b0f944e4e319dd3be44
evigian <35237884+evigian@users.noreply.github.com>
2018-07-12 17:32:33 +0300

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8080 // Change this to your server port
-    return `data/restaurants.json`;
+    return `./data/restaurants.json`;
   }
 
   /**
